a91feb9dad413f836c637f353ad5f27c06e3693d
Corowka <kopanchuke@gmail.com>
2023-02-14 01:25:10 +0300

    14.02.23v1

---
+++ self/Front-end/BBM/authorization/js/authorization.js
@@ -0,0 +1,13 @@
+function Auth_come() {
+    let bg = document.querySelector(".auth_page_bg");
+    let auth = document.querySelector(".auth ");
+    bg.style.display = "";
+    auth.style.display = "";
+}
+
+function Auth_left() {
+    let bg = document.querySelector(".auth_page_bg");
+    let auth = document.querySelector(".auth ");
+    bg.style.display = "none";
+    auth.style.display = "none";
+}
