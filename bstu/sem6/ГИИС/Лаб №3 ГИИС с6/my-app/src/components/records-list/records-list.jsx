d83d44dd754f8054e6c1900750d77b4aee2fb805
Corowka <107122474+Corowka@users.noreply.github.com>
2024-06-05 00:11:07 +0300

    commit

---
+++ bstu/sem6/ГИИС/Лаб №3 ГИИС с6/my-app/src/components/records-list/records-list.jsx
@@ -0,0 +1,34 @@
+import styles from "./records-list.module.css";
+import React from "react";
+import PropTypes from "prop-types";
+
+export function RecordsList({ recordPreviews, setRecord }) {
+  RecordsList.propTypes = {
+    recordPreviews: PropTypes.array,
+    setRecord: PropTypes.func,
+  };
+
+  return (
+    <div className={styles.wrap}>
+      {recordPreviews.map((record) => (
+        <button
+          data-test-id="audio-btn"
+          className={styles.recordItemContent}
+          key={record.key}
+          onClick={() => setRecord(record.key)}
+        >
+          <img
+            draggable={false}
+            className={styles.image}
+            src={record.image}
+            width={180}
+            height={180}
+            alt={record.name}
+          />
+          <div className={styles.name}>{record.name}</div>
+          <div className={styles.date}>{record.date}</div>
+        </button>
+      ))}
+    </div>
+  );
+}
